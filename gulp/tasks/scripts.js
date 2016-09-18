dd9cda50c63d836e232bc21eea7f6a9caeabe3ee
Aaron Bushnell <aaron.bushnell@gmail.com>
2016-09-17 21:34:59 -0400

    Add webpack basics

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,38 +1,10 @@
-console.log('test');
-/*
 import gulp from 'gulp';
-import browserify from 'browserify';
-import babelify from 'babelify';
-import source from 'vinyl-source-stream';
 import paths from '../paths';
 
 gulp.task('scripts:app', function () {
-  return browserify({
-    entries: paths.app.scriptsMain,
-    debug: true,
-    transform: ['browserify-handlebars']
-  })
-  .transform(babelify)
-  .bundle()
-  .on('error', function (err) {
-    console.log(err.toString());
-    this.emit('end');
-  })
-  .pipe(source('app.js'))
-  .pipe(gulp.dest(paths.app.tmpJSDir));
+  console.log('app');
 });
 
 gulp.task('scripts:vendor', function () {
-  return browserify({
-    entries: paths.app.scriptsVendor
-  })
-  .transform('babelify', { presets: ['es2015'] })
-  .bundle()
-  .on('error', function (err) {
-    console.log(err.toString());
-    this.emit('end');
-  })
-  .pipe(source('vendor.js'))
-  .pipe(gulp.dest(paths.app.tmpJSDir));
+  console.log('vendor');
 });
-*/
