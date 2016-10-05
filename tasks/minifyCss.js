bb50dc618917678f177d99f2156c677296def8ec
Aaron Bushnell <aaron.bushnell@gmail.com>
2016-10-04 23:53:05 -0400

    Update packages

--- tasks/minifyCss.js
+++
@@ -1,7 +0,0 @@
-// ./node_modules/.bin/cssnano ./dist/assets/_compiled/styles.css ./dist/assets/_compiled/styles.css
-var cssnano = require('cssnano');
-var fs = require('fs');
-
-cssnano.process(css, opts).then(function (result) {
-  fs.writeFileSync('./blah.css', result.css);
-});
