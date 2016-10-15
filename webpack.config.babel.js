62ffeea4ac52d453f3344989213112be306246b5
Aaron Bushnell <aaron.bushnell@gmail.com>
2016-10-15 00:02:36 -0400

    Add spacing

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,16 +5,19 @@ const PROD = process.env.NODE_ENV || 0;
 
 module.exports = {
   devtool: PROD ? false : "source-map",
+
   entry: {
     initial: './app/assets/scripts/Initial.js',
     home: './app/assets/scripts/Home.js'
   },
+
   output: {
     path: __dirname + '/app/assets/_compiled',
     publicPath: '/app/assets/_compiled',
     filename: '[name].js',
     chunkFilename: '[name]_[chunkhash].js'
   },
+
   plugins: PROD ? [
     new webpack.optimize.UglifyJsPlugin({
       output: {
@@ -22,6 +25,7 @@ module.exports = {
       }
     })
   ] : [],
+
   module: {
     loaders: [
       {
