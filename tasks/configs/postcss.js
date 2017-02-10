2f77ec59894e35f1b502861b2230e0a392725ceb
Aaron Bushnell <aaron.bushnell@gmail.com>
2017-02-09 22:46:34 -0500

    Update autoprefixer and use new format for defining browser versions

--- tasks/configs/postcss.js
+++ tasks/configs/postcss.js
@@ -10,9 +10,6 @@ module.exports = {
   ],
   "input": "./app/assets/styles/styles.css",
   "output": "./app/assets/_compiled/styles.css",
-  "autoprefixer": {
-    "browsers": "last 2 versions"
-  },
   "postcss-import": {
     glob: true
   }
