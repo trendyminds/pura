f57d16060af70cf51edc4586078349c4058aae3c
Aaron Bushnell <aaron.bushnell@gmail.com>
2017-02-11 20:56:33 -0500

    Setup eslint caching

--- tasks/subtasks/lint.sh
+++ tasks/subtasks/lint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 ./node_modules/.bin/stylelint './app/assets/styles/**/*.css' &&
-./node_modules/.bin/eslint './app/assets/scripts/**/*.js'
+./node_modules/.bin/eslint './app/assets/scripts/**/*.js' --cache
