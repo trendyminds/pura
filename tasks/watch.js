b2300f74bb0b85b4ddc4ec54a862bcbe5b115d67
Aaron Bushnell <aaron.bushnell@gmail.com>
2017-03-06 14:49:50 -0500

    Add external browsersync address

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,5 +1,6 @@
 const execa = require('execa');
 const Listr = require('listr');
+const os = require('os');
 
 const tasks = new Listr([
   {
@@ -23,7 +24,7 @@ const tasks = new Listr([
           task: () => execa.shell("node ./tasks/subtasks/watchSprite.js")
         },
         {
-          title: '🖥  Running Browsersync at http://localhost:3000',
+          title: `🖥  Running Browsersync at http://localhost:3000 and ${externalIp}`,
           task: () => execa("./tasks/subtasks/browsersync.sh")
         }
       ], { concurrent: true });
@@ -36,3 +37,19 @@ tasks.run().then(msg => {
 }).catch(err => {
   console.error(err.message);
 });
+
+/**
+ * Get IP address for external Browsersync address
+ */
+let interfaces = os.networkInterfaces();
+let ipAddresses = [];
+for (let k in interfaces) {
+  for (let k2 in interfaces[k]) {
+    let address = interfaces[k][k2];
+    if (address.family === 'IPv4' && !address.internal) {
+      ipAddresses.push(address.address);
+    }
+  }
+}
+
+const externalIp = `http://${ipAddresses[0]}:3000`;
