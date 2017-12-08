a20cf0003f998706a581505d9489620412616a49
Angeline Tran <angeline.e.tran@gmail.com>
2017-12-08 09:17:05 -0500

    change package

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,4 +1,9 @@
-require('es6-promise').polyfill();
+import Promise from 'promise-polyfill';
+
+// Add promise-polyfill to window
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 
 if (document.querySelectorAll('[data-my-module]').length) {
   System.import('./MyModule').then(module => new module.default());
