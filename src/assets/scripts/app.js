646322fb025aae30b7b5074b16dbcb6b76abcc42
Aaron Bushnell <aaron.bushnell@gmail.com>
2018-03-12 10:39:42 -0400

    Add  for easier DOM node loops

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,5 +1,6 @@
+import './Bling';
 import 'promise-polyfill/src/polyfill';
 
-if (document.querySelectorAll('[data-my-module]').length) {
+if ($('[data-my-module]').length) {
   System.import('./MyModule').then(module => new module.default());
 }
