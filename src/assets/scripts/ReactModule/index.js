c6309e0ce4dc9d6b514f6210108426bde3f1a33e
Aaron Bushnell <aaron.bushnell@gmail.com>
2018-06-01 21:24:28 -0400

    Test prettier on pre-commit

--- src/assets/scripts/ReactModule/index.js
+++ src/assets/scripts/ReactModule/index.js
@@ -1,17 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from 'react-dom';
 import PropTypes from "prop-types";
 
 const ReactModule = props => (
   <div>
     <h2>{props.greeting} world!</h2>
     <p>
-      I'm a React component. Pura comes with <strong>React</strong> and{" "}
-      <strong>ReactDOM</strong> installed by default.
+      I'm a React component. Pura comes with <strong>React</strong> and <strong>ReactDOM</strong> installed by default.
     </p>
     <p>
-      However, if you don't need it that's okay, too! Just don't{" "}
-      <code>import</code> it into your modules and you'll save on precious KB.
+      However, if you don't need it that's okay, too! Just don't <code>import</code> it into your modules and you'll save on precious KB.
     </p>
   </div>
 );
