d8963b476669975bee4eab1a257f44f3f826a2ad
Aaron Bushnell <aaron.bushnell@gmail.com>
2018-06-02 09:34:32 -0400

    Additional Prettier adjustments

--- tasks/subtasks/lint.sh
+++ tasks/subtasks/lint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 ./node_modules/.bin/stylelint './src/assets/styles/**/*.css' --color &&
-./node_modules/.bin/xo './src/assets/scripts/**/*.js' --color
+./node_modules/.bin/eslint './src/assets/scripts/**/*.js' --color
