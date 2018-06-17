ab8d237fb9998b7242e8e0792e781fcf75c45080
Aaron Bushnell <aaron.bushnell@gmail.com>
2018-06-17 10:11:29 -0400

    Allow JSX

--- webpack.config.js
+++ webpack.config.js
@@ -4,41 +4,39 @@ const PROD = process.env.NODE_ENV || 0;
 
 module.exports = {
   devtool: PROD ? false : "eval-cheap-module-source-map",
-
   entry: {
     app: "./src/assets/app.js",
     vendor: ["picturefill"]
   },
-
   output: {
     path: __dirname + "/src/_compiled",
     publicPath: "/_compiled/",
     filename: "[name].js",
     chunkFilename: "_chunk/[name]_[chunkhash].js"
   },
-
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
-      compress: PROD,
+      compress: PROD ? true : false,
       output: {
         comments: false
       }
     }),
-
     new webpack.optimize.CommonsChunkPlugin({
       name: "app",
       children: true,
       minChunks: 2
     })
   ],
-
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: "babel-loader"
       }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 };
