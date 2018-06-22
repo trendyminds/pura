d1c150355be0d51eb37079e14abf7c916f35d164
Aaron Bushnell <aaron.bushnell@gmail.com>
2018-06-22 08:19:42 -0400

    Remove comments

--- tasks/subtasks/revAssets.js
+++ tasks/subtasks/revAssets.js
@@ -4,13 +4,6 @@ const globby = require("globby");
 
 const assets = ["src/_compiled/*.css", "src/_compiled/*.js"];
 
-// globby(assets).then(paths => {
-//   new Version({
-//     assets: paths,
-//     grepFiles: [`./src${settings.templatePath}`]
-//   }).run();
-// });
-
 (async () => {
   const paths = await globby(assets);
 
